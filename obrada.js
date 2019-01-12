6a0f1c5a944cd7428333df2a1558753659ac1410
Damjan Pavlica <mudroljub@gmail.com>
2019-01-12 09:03:04 +0100

    cistka

--- obrada.js
+++ obrada.js
@@ -1,8 +1,10 @@
 const fs = require('fs')
 const ostalo = require('./data/ostalo.json')
 
-const filtrirano = ostalo.map(x => {
-  const index = x.text.indexOf("\n\nPortrayal in adaptations")
+const filtrirano = ostalo
+.filter(x => !x.title.startsWith('SR '))
+.map(x => {
+  const index = x.text.indexOf("\n\nAppearances in adaptations")
   if (index > 0) {
     x.text = x.text.substring(0, index)
   }
