5cdb127106902ea4f1686dff2983d2bb1d7a70a6
Damjan Pavlica <mudroljub@gmail.com>
2019-01-12 14:54:31 +0100

    pomalo

--- obrada.js
+++ obrada.js
@@ -2,13 +2,13 @@ const fs = require('fs')
 const ostalo = require('./data/ostalo.json')
 
 const filtrirano = ostalo
-.filter(x => !x.title.startsWith('SR '))
-.map(x => {
-  const index = x.text.indexOf("\n\nAppearances in adaptations")
-  if (index > 0) {
-    x.text = x.text.substring(0, index)
-  }
-  return x
-})
+.filter(x => x.title.toLowerCase().split(' ').includes('song'))
+// .map(x => {
+//   const index = x.text.indexOf("\n\nTrivia")
+//   if (index > 0) {
+//     x.text = x.text.substring(0, index)
+//   }
+//   return x
+// })
 
-fs.writeFileSync('ostalo.json', JSON.stringify(filtrirano, null, 2))
+fs.writeFileSync('pesme.json', JSON.stringify(filtrirano, null, 2))
