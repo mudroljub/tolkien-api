a99a3fb23e12ffc7c391ff7cc0e1d8d8765b00ee
Damjan Pavlica <mudroljub@gmail.com>
2019-01-13 23:44:25 +0100

    vraceni razmaci

--- alatke/povezivanje.js
+++ alatke/povezivanje.js
@@ -1,11 +1,11 @@
 const fs = require('fs')
-const podaci = require('./data/karakteri.json')
-const bekap = require('./data/bekap/stranice-bekap.json')
+const podaci = require('./data/ostalo.json')
+const bekap = require('./data/bekap/ostalo-bekap.json')
 
 const mapirano = podaci.map(k => {
-  const nadjeno = bekap.find(b => b.title._text == k.name)
+  const nadjeno = bekap.find(b => b.title == k.title)
   if (nadjeno) {
-    k.page_id = nadjeno.id._text
+    k.text = nadjeno.text
   }
   return k
 })
