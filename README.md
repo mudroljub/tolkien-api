4d0186d4b0f69588ba2c817f5e1d98f266f6d438
Damjan Pavlica <mudroljub@gmail.com>
2019-01-14 01:08:55 +0100

    pomalo

--- README.md
+++ README.md
@@ -2,5 +2,5 @@ http://lotr.wikia.com/?curid=27
 
 ## TODO
 
-- proveriti "Non-Canon Alert!" za sve karaktere
+- povuci infobox sa http://www.tolkiengateway.net/wiki/D%C3%BAnedain_of_Arnor
 - dodati u rase: Nazgûl, Wraiths
--- get-info.js
+++ get-info.js
@@ -1,27 +1,35 @@
 const fs = require('fs')
 const limit = require('simple-rate-limiter')
-const request = limit(require('request')).to(5).per(1000)
+const request = limit(require('request')).to(10).per(1000)
 const cheerio = require('cheerio')
 
 const podaci = require('./data/ostalo.json')
-// const mapirano = require('./mapirano.json')
-let mapirano = []
+const mapirano = require('./mapirano.json')
+// let mapirano = []
 console.log(podaci.length)
 
-let length = 2000
+let length = 600
 length = length > podaci.length ? podaci.length : length
-for (let i = 0; i < length; i++) {
+for (let i = 400; i < length; i++) {
   const item = podaci[i];
-  const url = item.lotrUrl ? item.lotrUrl : `http://lotr.wikia.com/?curid=${item.page_id}`
+  const url = `http://www.tolkiengateway.net/wiki/Thorin`
   request(url, function(error, response, body) {
     console.log(i, 'error:', error)
     const $ = cheerio.load(body)
-    const table = $('table')
+    const table = $('table[style^="float"] tbody')
     if (table) {
-      if (!(table.text()).includes("Non-Canon Alert!"))
-        mapirano.push(item)
-      else
-        console.log('nekanon')
+      const redovi = $(table.html())
+      redovi.map((i, red) => {
+        const celije = $(red).find('td')
+        if (celije.length == 2) {
+          let kljuc = $(celije[0]).text().toLowerCase()
+          kljuc = kljuc.split(' ').join('_')
+          const vrednost = $(celije[1]).html()
+          if (!kljuc in item)
+            item[kljuc] = vrednost
+        }
+      })
+    mapirano.push(item)
     }
     if (i == length - 1) fs.writeFileSync('mapirano.json', JSON.stringify(mapirano, null, 2))
   })
