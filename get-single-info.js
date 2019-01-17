37f3bb172c5bd9223c5127d45d43d09cb57e083f
Damjan Pavlica <mudroljub@gmail.com>
2019-01-17 23:37:13 +0100

    novoo

--- get-single-info.js
+++ get-single-info.js
@@ -7,7 +7,7 @@ const prevediPaSacuvaj = require('./prevedi-jedan')
 const bekap = require('./data/bekap/stranice-bekap.json')
 // const item = require('./item.json')
 
-const found = bekap.find(x => x.title._text == "Maeglin")
+const found = bekap.find(x => x.title._text == "Dwarves named \"Durin\"")
 const item = {
   name: found.title._text,
   lotr_page_id: found.id._text,
