6c3e54a838f8637ec4f2984fe48df4bd3de692ba
Damjan Pavlica <mudroljub@gmail.com>
2019-01-18 23:03:33 +0100

    m

--- alatke/obrada.js
+++ alatke/obrada.js
@@ -1,10 +1,10 @@
 const fs = require('fs')
-const podaci = require('../data/races.json')
+const podaci = require('../data/characters.json')
 
 const obradjeno = podaci
   .sort((a, b) => a.text.length - b.text.length)
   .map(x => {
-    if (!x.notable_members) {
+    if (!x.culture) {
       console.log(x.name)
     }
     return x
