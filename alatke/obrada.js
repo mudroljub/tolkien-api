383fb9b9967fdd4f0c0af4f195daa759bbb5019c
Damjan Pavlica <mudroljub@gmail.com>
2019-01-20 21:59:11 +0100

    cistka atributa

--- alatke/obrada.js
+++ alatke/obrada.js
@@ -1,14 +1,16 @@
 const fs = require('fs')
-const podaci = require('../data/locations.json')
+const podaci = require('../data/characters.json')
 
 const obradjeno = podaci
   .sort((a, b) => a.text.length - b.text.length)
   .map(x => {
-  //  if (!x.cultures && x.inhabitants) {
-  //    x.cultures = x.inhabitants
-  //    delete x.inhabitants
-  //  }
-    delete x.languages
+    //  if (!x.cultures && x.inhabitants) {
+    //    x.cultures = x.inhabitants
+    //    delete x.inhabitants
+    //  }
+    delete x.children
+    delete x.affiliation
+    delete x.language
     return x
   })
 
